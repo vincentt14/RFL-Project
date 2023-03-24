0bbf7894e8aacb399b90d68f769d5426feab2811
Vincent <vincent14@student.umn.ac.id>
2023-03-24 14:22:26 +0700

    fixing header

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -3,9 +3,8 @@
     <div class="flex flex-wrap">
       <div class="mb-12 w-full flex-col place-items-center font-medium md:flex md:w-1/3">
         <ul>
-          <h2 class="mb-5 text-4xl font-bold text-white selection:bg-blue-500 selection:text-blue-100">Dia<span
-              class="text-black">Care</span>.</h2>
-          <h3 class="mb-2 text-2xl font-bold text-white selection:bg-blue-500 selection:text-blue-100">Founder
+          <h2 class="mb-5 text-4xl font-bold text-black">RFL.</h2>
+          <h3 class="mb-2 text-2xl font-bold text-black">Founder
           </h3>
           <p class="text-white">Vincent</p>
           <p class="text-white">Rucci</p>
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,7 +3,7 @@
     <div class="relative flex items-center justify-between">
       <div class="px-4">
         <a href="/" class="block py-6 text-lg font-semibold text-secondary">
-          Dia<span class="text-primary">Care</span>.
+          RFL.
         </a>
       </div>
       <div class="flex items-center px-4">
@@ -13,27 +13,25 @@
           <span class="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
         </button>
         <nav id="nav-menu"
-          class="absolute right-4 top-full hidden w-full max-w-[250px] rounded-lg bg-white py-5 shadow-lg lg:static lg:block lg:max-w-full lg:rounded-none lg:bg-transparent lg:shadow-none">
-          <ul class="block lg:flex">
+          class="absolute right-4 top-full hidden w-full max-w-[250px] rounded-lg bg-stone-900 py-5 shadow-lg lg:static lg:block lg:max-w-full lg:rounded-none lg:bg-transparent lg:shadow-none">
+          <ul class="block text-secondary lg:flex">
             <li class="group">
-              <a href="/" class="text-dark mx-8 flex py-2 text-base group-hover:text-secondary">Home</a>
+              <a href="/" class="text-dark mx-8 flex py-2 text-base group-hover:text-primary">Home</a>
             </li>
             <li class="group">
-              <a href="/diagnose" class="text-dark mx-8 flex py-2 text-base group-hover:text-secondary">Diagnose</a>
+              <a href="/locations" class="text-dark mx-8 flex py-2 text-base group-hover:text-primary">Recycle
+                Locations</a>
             </li>
             <li class="group">
-              <a href="/dashboard" class="text-dark mx-8 flex py-2 text-base group-hover:text-secondary">Dashboard</a>
-            </li>
-            <li class="group">
-              <a href="/medicines" class="text-dark mx-8 flex py-2 text-base group-hover:text-secondary">Medicines</a>
+              <a href="/dashboard" class="text-dark mx-8 flex py-2 text-base group-hover:text-primary">Dashboard</a>
             </li>
             <li class="group">
               <a href="/about"
-                class="text-dark mx-8 mb-3 flex py-2 text-base group-hover:text-secondary lg:mb-0">About</a>
+                class="text-dark mx-8 mb-3 flex py-2 text-base group-hover:text-primary lg:mb-0">About</a>
             </li>
             <li class="group">
               <button
-                class="btnnn ml-5 rounded-sm border-2 border-black bg-black py-2 px-5 text-white duration-300 ease-out hover:bg-white hover:text-black">
+                class="ml-5 rounded-3xl border-2 border-secondary bg-black py-2 px-5 text-white duration-300 ease-out hover:bg-white hover:text-black">
                 <a href="/login">Login</a>
               </button>
             </li>
